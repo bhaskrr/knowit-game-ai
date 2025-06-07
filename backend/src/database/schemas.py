26efec9fb519f45c00faeae36d7d71e2eafaaae2
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-07 18:28:48 +0530

    added questionresponse and option schema

--- backend/src/database/schemas.py
+++ backend/src/database/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 # Response model for a topic
 class TopicResponse(BaseModel):
@@ -6,6 +7,20 @@ class TopicResponse(BaseModel):
     name: str
     description: str
     question_count: int
+    class Config:
+        from_attributes = True
+
+
+class Option(BaseModel):
+    id: int
+    text: str
+    is_correct: bool
+
+
+class QuestionResponse(BaseModel):
+    id: int
+    text: str
+    options: List[Option]
     
     class Config:
         from_attributes = True
