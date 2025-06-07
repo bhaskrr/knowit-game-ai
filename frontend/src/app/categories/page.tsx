7ced6e5efb051f6ed95799a4797b328f109c6cd9
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-07 12:51:59 +0530

    adjusted the category grids for better responsiveness

--- frontend/src/app/categories/page.tsx
+++ frontend/src/app/categories/page.tsx
@@ -106,7 +106,7 @@ export default function Index() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 px-6 py-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 px-6 py-4">
         {categories.map((category: Category) => (
           <CategoryCard key={category.id} {...category} />
         ))}
