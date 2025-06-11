3076ff85a6569a3a04ac807a66175f2c2f5140a5
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-11 22:42:18 +0530

    added forbidden words

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import find_dotenv, load_dotenv
+from backend.src.utils.forbidden_words import get_forbidden_words
 
 load_dotenv(find_dotenv())
 
 class Settings:
     DATABASE_URL = os.environ.get("DATABASE_URL")
     PROMPT_DIRECTORY = os.path.join("backend", "src", "prompt-templates")
+    FORBIDDEN_WORDS = get_forbidden_words()
 
 settings = Settings()
