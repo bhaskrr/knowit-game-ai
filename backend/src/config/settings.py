e2d9fe175763d156d83f530422d917a7afbeeace
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-12 17:16:50 +0530

    added reserved words

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 from backend.src.utils.forbidden_words import get_forbidden_words
+from backend.src.utils.reserved_words import get_reserved_words
 
 load_dotenv(find_dotenv())
 
@@ -8,5 +9,6 @@ class Settings:
     DATABASE_URL = os.environ.get("DATABASE_URL")
     PROMPT_DIRECTORY = os.path.join("backend", "src", "prompt-templates")
     FORBIDDEN_WORDS = get_forbidden_words()
+    RESERVED_WORDS = get_reserved_words()
 
 settings = Settings()
